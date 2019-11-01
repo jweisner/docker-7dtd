09ba68c2bd5842ad4672d96cf90a122030ce9697
Jesse Weisner <jesse_weisner@bcit.ca>
2019-11-01 12:16:45 -0700

    relocated steam install dir

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 APPID=294420
-FLAGFILE=/data/.upgrade_flag
+FLAGFILE=/steamlibrary/.upgrade_flag
 steamcmd=/home/steam/steamcmd/steamcmd.sh
 
-if [ ! -d /data/overrides ];then
+if [ ! -d /steamlibrary/overrides ];then
     echo "Creating overrides directory."
-    echo "Copy customized files from /data into /data/overides"
+    echo "Copy customized files from /steamlibrary into /steamlibrary/overides"
     echo "keeping the subpath intact. These files will replace"
     echo "game updates from Steam."
-    mkdir /data/overrides
+    mkdir /steamlibrary/overrides
 fi
 
 if [ -f ${FLAGFILE} ] && [ "${CHECK_UPDATE}" != "0" ];then
@@ -28,15 +28,15 @@ fi
 if [ -f ${FLAGFILE} ];then
     echo "Found flag file ${FLAGFILE}. Skipping game server install/upgrade check."
 else
-    if [ "${CONFIG_FILE}" = "/data/serverconfig.xml" ] && [ -f /data/serverconfig.xml ];then
+    if [ "${CONFIG_FILE}" = "/steamlibrary/serverconfig.xml" ] && [ -f /steamlibrary/serverconfig.xml ];then
         echo "Backing up ${CONFIG_FILE} into overrides..."
-        cp /data/serverconfig.xml /data/overrides/serverconfig.xml
+        cp /steamlibrary/serverconfig.xml /steamlibrary/overrides/serverconfig.xml
     fi
 
     echo "Installing/upgrading game server..."
     $steamcmd \
         +login anonymous \
-        +force_install_dir /data \
+        +force_install_dir /steamlibrary \
         +app_update ${APPID} validate \
         +quit
     echo -e "\n"
@@ -44,21 +44,21 @@ fi
 
 [ "${CHECK_UPDATE}" = "0" ] && touch ${FLAGFILE}
 
-if [ "${CONFIG_FILE}" != "/data/serverconfig.xml" ] && [ ! -f "${CONFIG_FILE}" ];then
+if [ "${CONFIG_FILE}" != "/steamlibrary/serverconfig.xml" ] && [ ! -f "${CONFIG_FILE}" ];then
     echo "Custom config file ${CONFIG_FILE} not found!" >&2
     exit 1
 fi
 
-if [ "${CONFIG_FILE}" = "/data/serverconfig.xml" ] && [ ! -f "${CONFIG_FILE}" ];then
+if [ "${CONFIG_FILE}" = "/steamlibrary/serverconfig.xml" ] && [ ! -f "${CONFIG_FILE}" ];then
     echo "Default config file not found. It will be created on"
     echo "the first startup. Stop the server after the first startup"
     echo "and edit ${CONFIG_FILE} with your local customizations."
 fi
 
 echo "Syncing override files..."
-rsync -av /data/overrides/ /data/ --exclude overrides/
+rsync -av /steamlibrary/overrides/ /steamlibrary/ --exclude overrides/
 
-/data/7DaysToDieServer.x86_64 \
+/steamlibrary/7DaysToDieServer.x86_64 \
   -logfile /dev/stdout \
   -configfile=${CONFIG_FILE} \
   -quit \
